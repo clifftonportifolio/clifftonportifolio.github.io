bc6f20ffcf254ba0027e64ed36a23dc52a600ddb
clifftonportifolio <engineerkapaga@gmail.com>
2025-11-21 12:28:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hero = document.getElementById("hero");
 const content = {
+    home: `<h1> I'm Cliffton Kapaga</h1>
+           <p>Aspiring software developer | Data analyst | Mechanical Engineer | Automation Enthusiast</p>`,
     about: `<h2>About Me</h2>
             <p>I build softwares and automated systems. i enjoy learning new technologies and applying them in real life</p>`,
     projects: `<h2>Projects</h2>
@@ -8,7 +10,8 @@ const content = {
                    <li><strong>Project Two</strong>simple calculator built with HTML, CSS & JS</li>
                </ul>`,
     contact: `<h2>Contact Me</h2>
-             <p>Email: engineerkapaga@gmail.com</p>`
+             <p>Email: engineerkapaga@gmail.com</p>
+             <p>whatsapp: +255711 384 611</p>`
 };
 const links = document.querySelectorAll('.nav-link');
 links.forEach(link => {
@@ -18,3 +21,4 @@ links.forEach(link => {
         hero.innerHTML = content[key];
     })
 })
+
